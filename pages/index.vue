7b3aba7bb84df0d982392e0e77bff65bf919f5c2
miyabiit <yamada.masahiro@gmail.com>
2020-05-06 19:05:24 +0900

    add google auth

--- pages/index.vue
+++ pages/index.vue
@@ -1,72 +1,56 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        task-wiz
-      </h1>
-      <h2 class="subtitle">
-        my first nuxt project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
-  </div>
+  <div class="wrapper">
+     <!-- header -->
+     <header class="header">
+       <!-- Loading -->
+       <div v-if="isWaiting" class="log_wait">
+         <p>読み込み中</p>
+       </div>
+       <!-- !login -->
+       <div class="isLogin_wrap" v-else>
+         <div v-if="!isLogin" class="btn_login">
+           <v-btn class="google_login" outlined @click="googleLogin">Googleでログイン</v-btn>
+         </div>
+         <!-- login -->
+         <div v-else class="log_wrap">
+           <v-btn class="google_logout" outlined @click="logOut">ログアウト</v-btn>
+         </div>
+       </div>
+     </header>
+     <!-- body -->
+   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
-export default {
-  components: {
-    Logo
-  }
-}
+  import firebase from "@/plugins/firebase";
+  export default {
+   asyncData() {
+     return {
+       isWaiting: true,
+       isLogin: false,
+       user: []
+     };
+   },
+   mounted: function() {
+     firebase.auth().onAuthStateChanged(user => {
+       this.isWaiting = false;
+       if (user) {
+         this.isLogin = true;
+         this.user = user;
+       } else {
+         this.isLogin = false;
+         this.user = [];
+       }
+     });
+   },
+   methods: {
+     googleLogin() {
+       const provider = new firebase.auth.GoogleAuthProvider();
+       firebase.auth().signInWithRedirect(provider);
+     },
+     logOut() {
+       firebase.auth().signOut();
+     }
+   }
+  };
 </script>
-
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
