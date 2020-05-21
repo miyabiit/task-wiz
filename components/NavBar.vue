0575e6434946f25ad59d5e2f596528c13ee89087
miyabiit <yamada.masahiro@gmail.com>
2020-05-21 19:54:42 +0900

    layout and css ajust

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,64 +1,65 @@
 <template>
-  <nav class="navbar has-background-light">
+  <nav class="navbar is-white" role="navigation" aria-label="main navigation">
     <div class="navbar-brand">
       <nuxt-link class="navbar-item" to="/">
-        <img src="https://bulma.io/images/bulma-logo.png" alt="Bulma" width="112" height="28">
+        TASK-WIZ
       </nuxt-link>
       <div class="navbar-burger burger" data-target="nabvarMenu" @click="showNav = !showNav" :class="{'is-active': showNav}">
         <span></span>
         <span></span>
-        <span></span>        
+        <span></span>
       </div>
     </div>
-    <div id="navBarTransparentExample" class="navbar-menu" :class="{'is-active': showNav}">
+    <div id="navMenu" class="navbar-menu" :class="{'is-active': showNav}">
       <div class="nav-end">
-        <nuxt-link class="navbar-item" to="/">
-          HOME
-        </nuxt-link>
         <div class="nabar-item has-dropdown is-hoverable">
           <div class="navbar-dropdown is-boxed">
+            <a class="button is-primary" v-if="isLoggedIn" @click="logout">Logout</a>
             <nuxt-link v-for="item in items" :key="item.title" :to="item.url" class="navbar-item">
               {{item.title}}
             </nuxt-link>
+            <a class="button is-light">{{username}}</a>
           </div>
         </div> 
-        <div class="navbar-item">
-          <p class="control">
-            <a class="button is-primary" href="https://github.com/jgthms/bulma/releases/download/0.7.0/bulma-0.7.0.zip">
-              <span class="icon">
-                <i class="fa fa-download"></i>
-              </span>
-              <span>Download</span>
-            </a>
-          </p>
-        </div>
       </div>
     </div>
   </nav>
 </template>
 
 <script>
-  import feature from "~/pages/feature.vue"
-  import contact from "~/pages/contact.vue"
+  //import about from "~/pages/about.vue"
+  //import templatePage from "~/pages/templatePage.vue"
+  import { mapState, mapGetters, mapActions } from "vuex"
+
+  export default {
+  }
   
   export default {
     components: {
-      feature,
-      contact
+      about,
+      templatePage
     },
     data: () => ({
       showNav: false,
       items: [
         {title: 'Home', icon: 'home', url: '/' },
-        {title: 'Feature', icon: 'code', url: '/feature'},
-        {title: 'Contact', icon: 'question_answer', url: '/contact'}
+        {title: 'About', icon: 'code', url: '/about'},
+        {title: 'Task Template', icon: 'question_answer', url: '/templatePage'}
       ]
-    })
+    }),
+    computed: {
+      ...mapState("auth", [
+        "username"
+      ]),
+      ...mapGetters("auth", [
+        "isLoggedIn"
+      ])
+    },
+    methods: {
+      ...mapActions("auth", ["logout"])
+    }
   }
 </script>
 
 <style>
-  .navbar {
-    margin-bottom: 20px;
-  }
 </style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,22 +1,17 @@
 <template>
   <div class="site">
-    <Header/>
     <nav-bar/>
-    <div class="siteContent">
-      <nuxt class="has-text-centered"/>
-    </div>
+    <nuxt/>
     <footer-bar/>
   </div>
 </template>
 
 <script>
-  import Header from '~/components/Header.vue'
   import FooterBar from '~/components/FooterBar.vue'
   import NavBar from '~/components/NavBar.vue'
   
   export default {
     components: {
-      Header,
       NavBar,
       FooterBar
     }
