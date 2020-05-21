f289462575f9639a8e7d706cdb6f8115937bc5bf
miyabiit <yamada.masahiro@gmail.com>
2020-05-22 07:37:32 +0900

    ajust navBar

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -2,7 +2,7 @@
   <nav class="navbar is-white" role="navigation" aria-label="main navigation">
     <div class="navbar-brand">
       <nuxt-link class="navbar-item" to="/">
-        TASK-WIZ
+        <strong>TASK-WIZ</strong>
       </nuxt-link>
       <div class="navbar-burger burger" data-target="nabvarMenu" @click="$store.commit('toggleMenu')" :class="{'is-active': $store.state.isMenuActive}">
         <span></span>
@@ -11,15 +11,15 @@
       </div>
     </div>
     <div id="navMenu" class="navbar-menu" :class="{'is-active': $store.state.isMenuActive}">
+      <div class="navbar-start">
+        <nuxt-link v-for="item in items" :key="item.title" :to="item.url" class="navbar-item">
+          {{item.title}}
+        </nuxt-link>
+      </div>
       <div class="nav-end">
-        <div class="nabar-item has-dropdown is-hoverable">
-          <div class="navbar-dropdown is-boxed">
-            <a class="button is-primary" v-if="isLoggedIn" @click="logout">Logout</a>
-            <nuxt-link v-for="item in items" :key="item.title" :to="item.url" class="navbar-item">
-              {{item.title}}
-            </nuxt-link>
-            <a class="button is-light">{{username}}</a>
-          </div>
+        <div class="nabar-item">
+          <a class="button is-light" v-if="isLoggedIn">{{username}}</a>
+          <a class="button is-primary" v-if="isLoggedIn" @click="logout">Logout</a>
         </div> 
       </div>
     </div>
